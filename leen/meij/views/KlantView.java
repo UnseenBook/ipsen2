6e4cc13e253bacde6d6256262cfbd05c74c7798c
Larada <info@mountainwebs.nl>
2013-10-31 12:51:27 +0100

    KlantView table header is not visible

--- leen/meij/views/KlantView.java
+++ leen/meij/views/KlantView.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Vector;
 
 import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
+import javax.swing.table.*;
 
 import leen.meij.*;
 
@@ -24,7 +24,7 @@ public class KlantView extends MasterView<ArrayList<Klant>>
 		
 		DefaultTableModel dtm = new DefaultTableModel();
 		dtm.addColumn("Klantnummer");
-		dtm.addColumn("Naam");
+		dtm.addColumn("Voornaam");
 		dtm.addColumn("Email");
 		dtm.addColumn("Telefoonnummer");
 		
@@ -32,24 +32,31 @@ public class KlantView extends MasterView<ArrayList<Klant>>
 		{
 			dtm.addRow(new Object[]{ 
 					klant.getKlantNummer(), 
-					klant.getVoornaam() +" " + klant.getTussenvoegsel() +" "+ klant.getAchternaam(),
+					klant.getVoornaam(),
 					klant.getEmailadres(),
 					klant.getTelefoonnummer(),
 			});
+			
 		}
 		
-		tblKlanten = new JTable(dtm);
+		TableColumnModel tcm = new DefaultTableColumnModel();
+		tcm.addColumn(new TableColumn(0, 50));
+		tcm.addColumn(new TableColumn(1, 200));
+		tcm.addColumn(new TableColumn(2, 200));
+		tcm.addColumn(new TableColumn(3, 200));
+		tcm.getColumn(0).setHeaderValue("ID");
+		tcm.getColumn(1).setHeaderValue("Voornaam");
+		tcm.getColumn(2).setHeaderValue("Email adres");
+		tcm.getColumn(3).setHeaderValue("Telefoonnummer");
 		
-		tblKlanten.setAutoscrolls(false);
-
-		tblKlanten.setAutoCreateColumnsFromModel(true);
 		
-		
+		tblKlanten = new JTable(dtm,tcm);
 		
 		
 		
 		
 		this.pnlBotMenu.add(this.btnWijzigen);
+		this.pnlContent.add(this.tblKlanten.getTableHeader(),"wrap");
 		this.pnlContent.add(this.tblKlanten);
 	}
 

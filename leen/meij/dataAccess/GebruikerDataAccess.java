9565295343073272da72352c0be19bfbbb4c8ba8
Daan Schoone <daanschoone@gmail.com>
2013-11-04 15:27:02 +0100

    GebruikerDataAccess werkt bij select(gebruikerID). nu nog de rest

--- leen/meij/dataAccess/GebruikerDataAccess.java
+++ leen/meij/dataAccess/GebruikerDataAccess.java
@@ -11,6 +11,31 @@ import leen.meij.utilities.DataAccess;
 
 public class GebruikerDataAccess extends DataAccess
 {
+	private PreparedStatement prepareStatement = null;
+	private ResultSet resultSet                = null;
+	private Gebruiker gebruiker;
+
+
+	private Gebruiker buildModel(ResultSet resultSet)
+	{
+		gebruiker = new Gebruiker();
+		try
+		{
+			while (resultSet.next())
+			{
+				gebruiker.setGebruikerID(resultSet.getInt("id"));
+				gebruiker.setPersoneelnummer(resultSet.getInt("personeelnummer"));
+				gebruiker.setGebruikersnaam(resultSet.getString("gebruikersnaam")); 
+				gebruiker.setWachtworod(resultSet.getString("wachtwoord"));
+				gebruiker.setAfdeling("afdeling placeholder");
+				gebruiker.setVoornaam(resultSet.getString("voornaam"));
+				gebruiker.setTussenvoegsel(resultSet.getString("tussenvoegsel"));
+				gebruiker.setAchternaam(resultSet.getString("achternaam"));
+				
+			}
+		} catch (SQLException negeer) {}
+		return gebruiker;
+	}
 
 	/**
 	 * 
@@ -21,21 +46,22 @@ public class GebruikerDataAccess extends DataAccess
 		openConnection();
 		try 
 		{
-			PreparedStatement ps = connection.prepareStatement("SELECT id, naam FROM gebruiker WHERE id = ?");
-			ps.setInt(1, gebruikerID);
-			ResultSet rs = ps.executeQuery();
-			while (rs.next())
-			{
-				System.out.println("ID: " + rs.getString("id"));
-				System.out.println("Naam: " + rs.getString("naam"));
-			}
-		} catch(SQLException sqle) 
+			prepareStatement = connection.prepareStatement("SELECT * FROM gebruiker WHERE id = ?");
+			prepareStatement.setInt(1, gebruikerID);
+			resultSet = prepareStatement.executeQuery();
+			gebruiker = buildModel(resultSet);
+		} catch (SQLException sqle)
 		{
 			sqle.printStackTrace();
+		} finally {
+		if (resultSet != null) try { resultSet.close(); } catch (SQLException logOrIgnore) {}
+		if (prepareStatement != null) try { prepareStatement.close(); } catch (SQLException logOrIgnore) {}
+		if (connection != null) try { connection.close(); } catch (SQLException logOrIgnore) {}
 		}
-		
+
 		// TODO - implement {class}.{operation}
-		throw new UnsupportedOperationException();
+		//throw new UnsupportedOperationException();
+		return new Gebruiker();
 	}
 
 	/**

d1b77640dcb9550a17ad32cca080490f7d663f31
Unseen Daan <daanschoone@gmail.com>
2014-01-03 15:26:17 +0100

    van lang geleden, weet niet wat ik heb gewijzigd

--- leen/meij/views/ReserveringDetailsView.java
+++ leen/meij/views/ReserveringDetailsView.java
@@ -3,8 +3,6 @@ package leen.meij.views;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
 import java.util.ArrayList;
 
 import javax.swing.JButton;
